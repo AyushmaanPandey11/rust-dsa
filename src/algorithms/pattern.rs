b4c1333cfbfa17845d8eb1fe9224a6f1ae94209d
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2025-10-31 15:47:02 +0530

    complex star patterns

--- src/algorithms/pattern.rs
+++ src/algorithms/pattern.rs
@@ -51,3 +51,83 @@ pub fn opposite_number_triangle(n : i32){
         println!();
     }
 }
+
+pub fn star_pyramid(n: i32){
+    for row in 0..n {
+        for _ in 1..n - row {
+            print!(" ");
+        }
+
+        for _ in 0..2*row+1 {
+            print!("*");
+        }
+
+        for _ in 0..n - row {
+            print!(" ");
+        }
+        println!();
+    }
+}
+
+pub fn opposite_star_pyramid(n: i32){
+    for row in 0..n {
+        for _ in 0..row {
+            print!(" ");
+        }
+
+        for _ in 0..2*(n -1 - row)+1 {
+            print!("*");
+        }
+
+        for _ in 0..row {
+            print!(" ");
+        }
+        println!();
+    }
+}
+
+pub fn combined_star_pyramid(n : i32){
+    for row in 0..n {
+        for _ in 1..n - row {
+            print!(" ");
+        }
+
+        for _ in 0..2*row+1 {
+            print!("*");
+        }
+
+        for _ in 0..n - row {
+            print!(" ");
+        }
+        println!();
+    }
+    for row in 0..n {
+        for _ in 0..row {
+            print!(" ");
+        }
+
+        for _ in 0..2*(n -1 - row)+1 {
+            print!("*");
+        }
+
+        for _ in 0..row {
+            print!(" ");
+        }
+        println!();
+    }
+}
+
+pub fn side_triangle(n : i32){
+    for row in 0..n+1 {
+        for _ in 0..row+1 {
+            print!("* ");
+        }
+        println!();
+    }
+    for row in 0..n+1 {
+        for _ in 0..n - row +1 {
+            print!("* ");
+        }
+        println!();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,8 @@ fn main() {
     algorithms::pattern::same_number_triangle(n);
     algorithms::pattern::opposite_triangle_star(n);
     algorithms::pattern::opposite_number_triangle(n);
+    algorithms::pattern::star_pyramid(n);
+    algorithms::pattern::opposite_star_pyramid(n);
+    algorithms::pattern::combined_star_pyramid(n);
+    algorithms::pattern::side_triangle(n);
 }
